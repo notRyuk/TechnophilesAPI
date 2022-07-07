4d36ae3e5ad166b34e36db1251917e72a7d090b9
sree valabh <amadeusrobot.notryuk@gmail.com>
2022-07-07 15:34:28 +0530

    Added updateEmail method for the UserDB

--- db.js
+++ db.js
@@ -1,11 +1,23 @@
 import mongoose from "mongoose";
 
-import {user, blog, ngo, UserBlogSchema} from "./mongoose.js";
+
+import { user, blog, ngo, UserBlogSchema } from "./mongoose.js";
+import { DB_URL } from "./config.js";
 
 mongoose.Promise = global.Promise
 
 Object.prototype.keys = () => Object.keys(this)
 Object.prototype.values = () => Object.values(this)
+
+try {
+    mongoose.connect(`${DB_URL}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+}
+catch {
+    console.log("Not able to connect to the database")
+}
 
 class CollectionObject {
     constructor(id, col) {
@@ -13,12 +25,57 @@ class CollectionObject {
         this.col = col
     }
 
+    /**
+     * Verifies if the user exists in the database
+     * @returns Boolean
+     */
     async verify() {
-        return await this.col.findById(this.id)
+        return (await this.col.findById(this.id))
     }
 
     __removeVersionInfo(obj) {
         delete obj.__v
+    }
+
+    /**
+     * 
+     * @returns Array<User|Blog|NGO> Objects
+     */
+    async findAll() {
+        var objects = await this.col.find()
+        for(var obj of objects) {
+            this.__removeVersionInfo(obj)
+        }
+        return objects
+    }
+
+    /**
+     * 
+     * @param {String} id ID of the User | Blog | NGO in the database
+     * @returns User|Blog|NGO Object from the database
+     */
+    async findById(id) {
+        var obj = this.col.findById(id)
+        this.__removeVersionInfo(obj)
+        return obj
+    }
+
+    async extractUserNames() {
+        var objects = await this.col.find()
+        var usernames = []
+        for(var obj of objects) {
+            usernames.push(obj._id)
+        }
+        return usernames
+    }
+
+    /**
+     * 
+     * @param {String} email The email ID to check the validation 
+     * @returns Boolean
+     */
+    isEmailValid(email) {
+        return /^[a-z][a-z0-9_\.-]+\@[a-z0-9\-]+\.[a-z]+/.test(email)
     }
 }
 
@@ -26,14 +83,16 @@ class CollectionObject {
 class UserObject extends CollectionObject {
     /**
      * 
-     * @param {string} id ID of the user to be saved in the database
-     * @param {string} firstName First Name of the user to be saved in the database
-     * @param {string} lastName Last Name of the user to be saved in the database
-     * @param {string} encryption Password of the user to be saved in the database
-     * @param {string} email Email of the user to be saved in the database
+     * @param {String} id ID of the user to be saved in the database
+     * @param {String} firstName First Name of the user to be saved in the database
+     * @param {String} lastName Last Name of the user to be saved in the database
+     * @param {String} encryption Password of the user to be saved in the database
+     * @param {String} email Email of the user to be saved in the database
      * @param {UserBlogSchema[]} blogs List of Simplified blogs to be saved in the database
      */
     constructor(id, firstName, lastName, encryption, email, blogs) {
+        super(id, user)
+
         this.col = user
         this.id = id
         this.firstName = firstName
@@ -41,25 +100,10 @@ class UserObject extends CollectionObject {
         this.encryption = encryption
         this.email = email
         this.blogs = blogs
-
-        super(this.id, this.col)
     }
 
-    __hasId() {
-        return this.id?true:false
-    }
-
-    /**
-     * 
-     * @param {string} id ID of the user to be saved in the database
-     * @param {string} firstName First Name of the user to be saved in the database
-     * @param {string} lastName Last Name of the user to be saved in the database
-     * @param {string} encryption Password of the user to be saved in the database
-     * @param {string} email Email of the user to be saved in the database
-     * @returns UserObject
-     */
-    async create(id, firstName, lastName, encryption, email) {
-        if(this.__hasId()) {
+    async createUser() {
+        if(await this.verify()) {
             return new UserObject(
                 this.id,
                 this.lastName,
@@ -68,13 +112,54 @@ class UserObject extends CollectionObject {
                 this.blogs
             )
         }
-        var __user = new user({
-            _id: id,
+        if(!this.isEmailValid(this.email)) {
+            return {
+                status: 404,
+                error: "BadRequest! The given email is not valid!",
+                comment: "Use static method to change the email or dynamically change the email if not valid!"
+            }
+        }
+        var __user = await (new user({
+            _id: this.id,
             name: {
-                first: firstName,
-                last: lastName
+                first: this.firstName,
+                last: this.lastName
             },
-            encryption: encryption
-        })
+            encryption: this.encryption,
+            email: this.email
+        })).save()
+        var __user = __user._doc
+        this.__removeVersionInfo(__user)
+        return new UserObject(
+            __user.id,
+            __user.firstName,
+            __user.lastName,
+            __user.encryption,
+            __user.email,
+            []
+        )
+    }
+    
+    async updateEmail(newEmail) {
+        var __user = await this.verify()
+        if(!__user) {
+            return {
+                status: 400,
+                comment: "The requested id is not in the database"
+            }
+        }
+        if(!this.isEmailValid(newEmail)) {
+            return {
+                status: 400,
+                comment: "The mentioned email is not valid"
+            }
+        }
+        __user = (await __user.set("email", newEmail).save())._doc
+        this.__removeVersionInfo(__user)
+        console.log(__user)
+        this.email = newEmail
+        return this
     }
 }
+
+console.log(await new UserObject("1", "Test", "Test", "Alpha", "abc@def.ghi", []).updateEmail("lambda.epsilon@beta.com"))
