1cdbbe0a5c69851569cc7830feb4c727fd3d72ee
Sreevallabh Karanam <amadeusrobot.notryuk@gmail.com>
2022-08-11 14:30:39 +0530

    Added the tokenizer function.

--- helpers.js
+++ helpers.js
@@ -39,6 +39,101 @@ const searchBlog = async(query, text) => {
     return out
 }
 
+function List(array) {
+    return new Proxy(array, {
+        get: (target, prop, rec) => {
+            if(prop.split(",").length > 1) {
+                return prop.split(",").map( e => rec[p])
+            }
+            if(prop < 0) {
+                return Reflect.get(target, String(target.length+parseInt(prop, 10), rec))
+            }
+            return Reflect.get(target, prop.at, rec)
+        },
+        set: (target, prop, value, rec) => {
+            if(prop < 0) {
+                return Reflect.set(target, String(target.length+parseInt(prop, 10)), value, rec)
+            }
+            return Reflect.set(target, prop, value, rec)
+        }
+    })
+}
+
+class Tokenizer {
+    /**
+     * Tokenize the text into a token for multiple other purposes
+     * @param {String} text The text that has to be tokenized
+     * @returns String
+     */
+    encrypt(text) {
+        var year = new Date().getFullYear()
+        var key = Math.floor((text.charCodeAt(0)*text.charCodeAt(1)+16)/year)
+        var text = this.shuffle(text.split("").map((e, i) => (e.charCodeAt(0)+key).toString().padStart(3, "0")+(i+key).toString().padStart(2, "0"))).join("")
+        year = year.toString().split("").map((e, i) => (i===0)?e.charCodeAt(0)-key:e.charCodeAt(0)+key).join("")
+        text += ":"+year+key.toString()+"-"+key.toString().length
+        return text
+    }
+
+    /**
+     * Decrypts the given token into string
+     * @param {String} token The token that is to be converted back into the text
+     * @returns String
+     */
+    decrypt(token) {
+        var tokens = token.split("-")
+        var keyLength = Number(tokens[tokens.length-1])
+        var mainTokens = tokens[0].split(":")
+        var temp = mainTokens[1].split("").reverse()
+        var key = 0
+        var i = 0
+        while(i<keyLength) {
+            key += Number(temp.splice(0, 1))*(10**i)
+            i++
+        }
+        key = Number(key)
+        var keys = []
+        var temp1 = mainTokens[0].split("")
+        for(var i=0; i<mainTokens[0].length/5; i++) {
+            temp = []
+            for(var k=0; k<5; k++) {
+                temp.push(temp1.splice(0, 1)[0])
+            }
+            keys.push(temp)
+        }
+        var values = Array(keys.length).fill("")
+        for(var k of keys) {
+            var ascii = 0
+            var index = 0
+            i = 3
+            while(i--) {
+                ascii += (+k.splice(0, 1))*(10**i)
+            }
+            i = 2
+            while(i--) {
+                index += (+k.splice(0, 1))*(10**i)
+            }
+            values[index-key] = String.fromCharCode(ascii-key)
+        }
+        return values.join("")
+    }
+
+    /**
+     * Returns the shuffled array
+     * @param {String[]} array The strings that have to be shuffled
+     * @returns String[]
+     */
+    shuffle(array) {
+        var m = array.length, t, i
+        while(m) {
+            i = Math.floor(Math.random()*m--)
+            t = array[m]
+            array[m] = array[i]
+            array[i] = t
+        }
+        return array
+    }
+}
+
 class Globals {
     constructor() {
         setImmediate(() => {
@@ -160,11 +255,13 @@ const stateList = [
 ]
 
 const __globals = new Globals()
+const tokenizer = new Tokenizer()
 
 export {
     lowerCase,
     createLowerCaseObject,
     searchBlog,
     __globals,
-    stateList
+    stateList,
+    tokenizer
 }
